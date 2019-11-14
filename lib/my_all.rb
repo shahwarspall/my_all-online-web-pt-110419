39059d3a6c79975c3c5bbfb017150e8870fb3c6c
Shahwar Spall <shevy.spall@gmail.com>
2019-11-14 04:28:39 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,7 @@ def my_all?(collection)
   block_return_values= []
   while i < collection.length 
     
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i = i +1 
 
 end
